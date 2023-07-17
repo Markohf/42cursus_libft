d805a9348f24295069c0c79544d23df968053bd5
Marco Andree Fernandini Limo <marco-fe@c1r12s4.42madrid.com>
2023-07-17 12:09:06 +0200

    last

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: marco-fe <marco-fe@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 18:29:39 by marco-fe          #+#    #+#             */
-/*   Updated: 2023/04/12 15:37:45 by marco-fe         ###   ########.fr       */
+/*   Updated: 2023/07/17 12:07:10 by marco-fe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,6 @@
 # include <string.h>
 # include <strings.h>
 # include <stdio.h>
-
-typedef struct s_list
-{
-	void			*content;
-	struct s_list	*next;
-}	t_list;
 
 /*PART 1: recreating C functions.*/
 int		ft_isalpha(int val);
@@ -63,16 +57,5 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-
-/*BONUS: creating list functions.*/
-t_list	*ft_lstnew(void *content);
-void	ft_lstadd_front(t_list **lst, t_list *new);
-int		ft_lstsize(t_list *lst);
-t_list	*ft_lstlast(t_list *lst);
-void	ft_lstadd_back(t_list **lst, t_list *new);
-void	ft_lstdelone(t_list *lst, void (*del)(void *));
-void	ft_lstclear(t_list **lst, void (*del)(void *));
-void	ft_lstiter(t_list *lst, void (*f)(void *));
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
