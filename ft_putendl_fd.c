05ce193cbda4b541c07b34b89024ecbd91c0d956
Marco Andree Fernandini Limo <marco-fe@c1r10s1.42madrid.com>
2023-07-26 11:30:49 +0200

    ft_putendl_fd.c fixed

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: marco-fe <marco-fe@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 12:39:57 by marco-fe          #+#    #+#             */
-/*   Updated: 2023/04/08 12:49:35 by marco-fe         ###   ########.fr       */
+/*   Updated: 2023/07/26 11:28:31 by marco-fe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,6 @@
 void	ft_putendl_fd(char *s, int fd)
 {
 	if (s)
-	{
 		write(fd, s, ft_strlen(s));
-		write(fd, "\n", 1);
-	}
+	write(fd, "\n", 1);
 }
